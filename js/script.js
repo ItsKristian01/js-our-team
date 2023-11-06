c9ce11fcfa26f4a98d322389df4db39c60808420
ItsKristian01 <kristianhana11@gmail.com>
2023-11-06 15:51:41 +0100

    add: array of objects

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const teamMembers = [
+    {
+        name: "Wayne Barnett",
+        profession: "Founder & CEO",
+        image: "wayne-barnett-founder-ceo.jpg"
+    },
+   {
+    name: "Angela Caroll",
+    profession: "Chief Editor",
+    image: "angela-caroll-chief-editor.jpg"
+   },
+   {
+     name: "Walter Gordon",
+     profession: "Office Manager",
+     image: "walter-gordon-office-manager.jpg"
+   },
+   {
+     name: "Angela Lopez",
+     profession: "Social Media Manager",
+     image: "angela-lopez-social-media-manager.jpg"
+   },
+   {
+     name: "Scott Estrada",
+     profession: "Developer",
+     image: "scott-estrada-developer.jpg"
+   },
+   {
+     name: "Barbara Ramos",
+     profession: "Graphic Designer",
+     image: "barbara-ramos-graphic-designer.jpg"
+   }
+]
+
+console.log(teamMembers);
+  
